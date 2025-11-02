ed68172aa2b8ea07ad505bb7fc82f39d097d9dca
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2025-11-02 00:28:46 +0000

    update

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const lession1Controllker = require('./controllers/lession1');
+const lesson1Controller = require('./controllers/lesson1');
  
-app.get('/', lession1Controller.kathleenRoute);
-app.get('/angela', lession1Controller.angelaRoute);
+app.get('/', lesson1Controller.kathleenRoute);
+app.get('/angela', lesson1Controller.angelaRoute);
 
 const port = 3000;
  
