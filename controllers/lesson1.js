5b01791d7313fc0022f812b23402872af75dce77
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2025-11-02 01:10:20 +0000

    going above and beyond for first assignment for 100%

--- controllers/lesson1.js
+++ controllers/lesson1.js
@@ -6,7 +6,12 @@ const angelaRoute = (req, res) => {
   res.send('Angela Crisp');
 };
 
+const nephiRoute = (req, res) => {
+  res.send('Nephi Wannenburgh');
+};
+
 module.exports = {
   kathleenRoute,
   angelaRoute,
+  nephiRoute,
 };
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const lesson1Controller = require('../controllers/lesson1');
  
 routes.get('/', lesson1Controller.kathleenRoute);
 routes.get('/angela', lesson1Controller.angelaRoute);
+routes.get('/nephi', lesson1Controller.nephiRoute);
 
 module.exports = routes;
