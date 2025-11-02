816a7b83b32cb888f16201a4626436c32c75776c
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2025-11-02 23:13:15 +0000

    update

--- controllers/lesson1.js
+++ controllers/lesson1.js
@@ -1,9 +1,13 @@
 const kathleenRoute = (req, res) => {
-  res.send('Kathleen Mclellan');
+  res.send('Kathleen Mclellan, ');
 };
 
 const angelaRoute = (req, res) => {
-  res.send('Angela Crisp');
+  res.send(
+    'Name: Angela Crisp' <br>
+    'Email: acrisp@gmail.com' <br>
+    'Favourite Colour: Lilac'<br>
+    'Birthdate: 20th November 1936'); 
 };
 
 const nephiRoute = (req, res) => {
